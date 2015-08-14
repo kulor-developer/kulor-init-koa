37e6a1ef8acb3e3d2fb502dd4aeb68442371554b
bo.cheng <chen8114@sina.com>
2015-08-14 12:00:22 +0800

    update file

--- kulorBuild.js
+++ kulorBuild.js
@@ -5,7 +5,12 @@ module.exports  = function( bower , grunt , tool , log , callback ) {
     bower.commands
         .install( [ "zepto" ] , { save : true } )
         .on( "end" , function( installed ){
+            tool.file.copy( path.resolve( self.bowerDir , "zepto/zepto.min.js" ) , path.resolve( self.cwd , "src/js/lib/zepto.js" ) );
+            tool.file.copy( path.resolve( __dirname , "grunt" ) , path.resolve( self.cwd ) );
+            tool.file.copy( path.resolve( __dirname , "site" ) , path.resolve( self.cwd ) );
+            tool.file.copy( path.resolve( __dirname , "src" ) , path.resolve( self.cwd ) );
             log( "kulor-koa-app init success" );
             callback();
         } );
+    callback();
 }
